1bb50c61523d0848a2d0991f45a4e4bc1c4596fd
simonchen333 <50697911+simonchen333@users.noreply.github.com>
2022-07-06 19:15:53 +0800

    Add files via upload

---
+++ Hand-base/WiFi/04Seg_TrainSiamese_StateInference/02LoadModel_StateInference/2siamese_test_hand_WIFI.bat
@@ -0,0 +1,15 @@
+
+
+
+
+if 1==1 (
+	
+	python siamese_test_hand_WIFI.py --csiFile user1_ph_6.mat 
+	python siamese_test_hand_WIFI.py --csiFile user1_rp_6.mat 
+	python siamese_test_hand_WIFI.py --csiFile user1_sd_6.mat 
+	python siamese_test_hand_WIFI.py --csiFile user1_wd_6.mat 
+)
+
+
+pause
+
